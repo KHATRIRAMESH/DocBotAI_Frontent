25f861006d7a52504eb6bf3a496d17e5bbfdb762
Nischal <nyyscal541556@gmail.com>
2025-05-30 17:49:25 +0545

    Admin Layout Updated

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -3,20 +3,19 @@
 import AdminDashboardSideBar from "../../components/admin/AdminDashboardComponent";
 import { useUser } from "@clerk/nextjs";
 
-export default function DashboardLayout({
-  children,
-}) {
+export default function DashboardLayout({ children }) {
   const { user } = useUser();
   console.log(user?.primaryEmailAddress?.emailAddress);
 
   return (
-    <div className="flex min-h-screen">   
-      <aside className="shrink-0">         
+    <div className="flex min-h-screen">
+      {/* Fixed Sidebar */}
+      <aside className="shrink-0 h-screen sticky top-0 overflow-y-auto border-r border-gray-200">
         <AdminDashboardSideBar />
       </aside>
 
-      {/* ----- Page content ----- */}
-      <main className="flex-1 overflow-y-auto bg-[#F5F5F5]">
+      {/* Scrollable Main Content */}
+      <main className="flex-1 overflow-y-auto bg-[#F5F5F5] min-h-screen">
         {children}
       </main>
     </div>
